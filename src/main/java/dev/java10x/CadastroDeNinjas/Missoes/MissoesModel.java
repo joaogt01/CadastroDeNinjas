7050ae512900ca4f819a758dd21408d37eb182c3
João Victor <joao.dev06@gmail.com>
2025-09-06 21:59:02 -0300

    criei a função de missoes no sistema

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -0,0 +1,49 @@
+package dev.java10x.CadastroDeNinjas.Missoes;
+
+import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_missoes")
+public class MissoesModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nome;
+    private String rank;
+    private NinjaModel ninja;
+
+    public MissoesModel() {
+    }
+
+    public MissoesModel(long id, String nome, String rank) {
+        this.id = id;
+        this.nome = nome;
+        this.rank = rank;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+}
--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -1,6 +1,9 @@
-package dev.java10x.CadastroDeNinjas;
+package dev.java10x.CadastroDeNinjas.Ninjas;
 
+import dev.java10x.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+
+import java.util.List;
 
 
 @Entity
@@ -13,6 +16,7 @@ public class NinjaModel {
     private String nome;
     private String email;
     private int idade;
+    private List<MissoesModel> missoes;
 
     public NinjaModel() {
     }
