cd6988da9a98d76fff5d48bd9d46d3d42cb923e9
João Victor <joao.dev06@gmail.com>
2025-09-16 21:08:08 -0300

    feat: altera a rota do controller para listar missoes e listar por Id e injeta a dependencia do SERVICE e do REPOSITORY

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
@@ -1,15 +1,43 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping
+@RequestMapping("/missoes")
 public class MissoesController {
 
-    @GetMapping("/missoes")
-    public String ControllerMissoes(){
-        return "Essa é a tabela de Missoes";
+    private MissoesService missoesService;
+
+    public MissoesController(MissoesService missoesService) {
+        this.missoesService = missoesService;
     }
+
+    @PostMapping("/criar")
+    public String criarMissao(){
+        return "Missao Criada";
+    }
+
+    @GetMapping("/listar")
+    public List<MissoesModel> listarNinjas(){
+        return MissoesService.listarMissoes();
+    }
+
+    @GetMapping("/listar/{id}")
+    public MissoesModel ListarMissoesPorId(@PathVariable Long id){
+        return missoesService.listarMissoesId(id);
+    }
+
+    @PutMapping("/alterarID")
+    public String alterarMissoesPorId(){
+        return "Alterar Missoes Por Id";
+    }
+
+    @DeleteMapping("/deletarID")
+    public String deletarMissaoPorId(){
+        return "Deletar Missao Por Id";
+    }
+
 }
--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesService.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesService.java
@@ -1,4 +1,29 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class MissoesService {
+
+    private static MissoesRepository missoesRepository;
+
+    public MissoesService(MissoesRepository missoesRepository) {
+        this.missoesRepository = missoesRepository;
+    }
+
+    //listar todas as missoes
+    public static List<MissoesModel> listarMissoes(){
+        return missoesRepository.findAll();
+    }
+
+    //listar missoes por id
+    public MissoesModel listarMissoesId(Long id){
+        Optional<MissoesModel> missaoPorId = missoesRepository.findById(id);
+        return missaoPorId.orElse(null);
+    }
+
+
 }
