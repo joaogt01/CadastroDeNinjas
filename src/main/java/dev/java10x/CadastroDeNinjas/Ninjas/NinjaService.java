2da3cac871fc49dff8595d12d6b115b27399964d
João Victor <joao.dev06@gmail.com>
2025-09-17 21:21:02 -0300

    feat: inclui a função de adicionar Ninja no Banco de Dados

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaService.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaService.java
@@ -25,4 +25,8 @@ public class NinjaService {
         return ninjaPorId.orElse(null);
     }
 
+    //Criar um novo Ninja
+    public NinjaModel criarNinja(NinjaModel ninja){
+        return ninjaRepository.save(ninja);
+    }
 }
